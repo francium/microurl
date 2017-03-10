dd52ba444fead0a374de3de03b73307cc33c3a23
francium <varunhooda1@gmail.com>
2017-03-10 18:00:42 -0500

    First letter of micro is now lower case.

--- random_micro.py
+++ random_micro.py
@@ -6,8 +6,17 @@ RANDOM_WORD_API_PARAMS = {'len': 5}
 
 
 def random(length):
-    return ''.join([get_word().capitalize() for i in range(length)])
+    '''
+        Return a string of random words concatenated together.
+    '''
+    words = [get_word().capitalize() for i in range(length)]
+    # Make first letter of first word lower case
+    words[0] = words[0][0].lower() + words[0][1:]
+    return ''.join(words)
 
 
 def get_word(url=RANDOM_WORD_API_URL, params=RANDOM_WORD_API_PARAMS):
+    '''
+        Get a random word.
+    '''
     return requests.get(url, params=params).text
